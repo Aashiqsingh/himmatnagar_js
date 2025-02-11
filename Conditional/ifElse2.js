b0ed46487e28eaf2897d26dc679abc653903f291
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-11 15:31:52 +0530

    loop

---
+++ Conditional/ifElse2.js
@@ -0,0 +1,20 @@
+var a = parseInt(prompt("Enter first number "))
+var b = parseInt(prompt("Enter second number "))
+var c = parseInt(prompt("Enter third number "))
+
+var sum = 0;
+
+if(a > 35)
+{
+    sum += a;
+}
+if(b > 35){
+    sum += b;
+}
+if(c > 35)
+{
+    sum += c;
+}
+
+console.log(sum);
+
