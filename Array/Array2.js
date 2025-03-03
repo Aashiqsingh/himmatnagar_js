e3df97d68cd93cac2159f1f1b30296b73baa7860
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-03 15:10:01 +0530

    array

---
+++ Array/Array2.js
@@ -0,0 +1,41 @@
+var arr = ["ram","seeta","geeta","mohan","krishna","radha"];
+
+
+console.log(arr);
+
+// var x = arr.splice(3,1)
+// var x = arr.splice(3,0,"rahul")
+// var x = arr.splice(3,2,"rahul","tejas")
+
+// console.log(x);
+
+// console.log(arr);
+
+
+
+
+
+
+// var x = arr.includes("mohon")
+// console.log(x);
+
+// var x = arr.join("$")
+// var x = arr.join("_")
+// var x = arr.join(" ")
+// var x = arr.join("")
+
+// console.log(x);
+
+// var x = arr.reverse()
+// console.log(x);
+
+// var x = arr.slice(1)
+// var x = arr.slice(1,4)
+
+// console.log(x);
+
+// var x = arr.sort()
+// console.log(x);
+
+
+
