f47fb76cede6f19741efa13d7e5903fcf6a06d02
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-04 15:42:23 +0530

    array json 3

---
+++ JsonArray/array2.js
@@ -0,0 +1,46 @@
+var products = [
+    {
+        id:101,
+        name:"Iphone",
+        category:"Mobile",
+        price:100000,
+        color:["red","black","blue","green","yellow"],
+        isAvailable:true,
+        details:{
+            description:"Apple iPhone 13 Pro Max",
+            weight:1500,
+            screenSize:6.1,
+            batteryLife:48,
+        }
+    },
+    {
+        id:102,
+        name:"Samsung Galaxy S21 Ultra",
+        category:"Mobile",
+        price:75000,
+        color:["white","black","blue","green"],
+        isAvailable:false,
+        details:{
+            description:"Samsung Galaxy S21 Ultra 5G",
+            weight:1600,
+            screenSize:6.7,
+            batteryLife:45,
+        }
+
+    },
+    {
+        id:103,
+        name:"Dell laptop",
+        category:"Laptop",
+        price:85000,
+        color:["white","black","blue","aqua"],
+        isAvailable:false,
+        details:{
+            description:"widows with tablets",
+            weight:1400,
+            screenSize:6.4,
+            batteryLife:42,
+        }
+
+    }
+]
