753dcf13cec12338772522da56ff0511c4e6df1d
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-11 15:39:19 +0530

    dom manipulate

---
+++ DOM/dom3.js
@@ -0,0 +1,11 @@
+function changeSize(){
+    var mydiv = document.getElementById("mydiv")
+    mydiv.style.height = "400px";
+    mydiv.style.width = "400px";
+    // mydiv.style.backgroundColor = "green";
+    mydiv.style.borderRadius = "50%";
+    mydiv.style.border = "5px dotted blue"
+    mydiv.style.backgroundImage = "url('https://cdn.britannica.com/64/171264-050-C7253301.jpg')"
+    mydiv.style.backgroundSize = "cover";
+    mydiv.style.transition = "1s"
+}
